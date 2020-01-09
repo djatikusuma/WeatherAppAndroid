40b55a120f72f2d23c4fabc3e183e1605353810b
Rangga Djatikusuma Lukman <rangga.djatikusuma@bigio.id>
2020-01-09 13:01:47 +0700

    Konfigurasi API Openwheater

---
+++ app/src/main/java/com/codekinian/wheaterapp/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.codekinian.wheaterapp.utils
+
+import com.codekinian.wheaterapp.BuildConfig
+
+class Constants {
+    companion object {
+        const val BASE_URL = BuildConfig.BASE_URL
+        const val IMG_URL = BuildConfig.IMG_URL
+        const val API_KEY = BuildConfig.API_KEY
+    }
+}
